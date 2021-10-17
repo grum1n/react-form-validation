bcb6a05f2914b6a6bcd1e17e58d5d17e8cb02694
grum1n <grumin.el.pastas@gmail.com>
2021-10-17 13:57:50 +0300

    added form code  to FormSignup.js

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -3,7 +3,63 @@ import React from 'react';
 function FormSignUp() {
     return (
         <div>
-            <h1>Sign up</h1>
+            <div className="form-content-right">
+               <form className="form">
+                   <h1>Get started with us today !  Create your account by filling out the information below .</h1>
+                   <div className="form-inputs">
+                        <label htlmFor="username" className="form-label">
+                            Username 
+                        </label>
+                        <input 
+                            id="username"
+                            type="text" 
+                            name="username" 
+                            className="form-input"
+                            placeholder="Enter your username"
+                        />
+                   </div>
+                   <div className="form-inputs">
+                        <label htlmFor="email" className="form-label">
+                            Email
+                        </label>
+                        <input 
+                            id="email"
+                            type="email" 
+                            name="email" 
+                            className="form-input"
+                            placeholder="Enter your email"
+                        />
+                   </div>
+                   <div className="form-inputs">
+                        <label htlmFor="password" className="form-label">
+                            Password
+                        </label>
+                        <input 
+                            id="password"
+                            type="password" 
+                            name="password" 
+                            className="form-input"
+                            placeholder="Enter your password"
+                        />
+                   </div>
+                   <div className="form-inputs">
+                        <label htlmFor="password2" className="form-label">
+                            Confirm Password
+                        </label>
+                        <input 
+                            id="password2"
+                            type="password2" 
+                            name="password2" 
+                            className="form-input"
+                            placeholder="Enter your password2"
+                        />
+                   </div>
+                   <button className="form-input-btn" type="submit">Sign Up</button>
+                   <span className="form-input-login">
+                        Already have an account ? Login <a href="#"> here</a>
+                   </span>
+               </form>
+            </div>
         </div>
     )
 }
