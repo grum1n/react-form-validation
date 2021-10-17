88d82506f4ff627f11470ee698e2aea8b1c91f8c
grum1n <grumin.el.pastas@gmail.com>
2021-10-17 13:26:53 +0300

    added function to Form.js and Form Signup.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
-      <h1>testas</h1>
+      <Form />
     </div>
   );
 }
