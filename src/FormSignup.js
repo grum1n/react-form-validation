958ef0ce56204cf2a52abef90a4c23d19bc0b067
grum1n <grumin.el.pastas@gmail.com>
2021-10-17 18:10:24 +0300

    end tutorial

--- src/FormSignup.js
+++ src/FormSignup.js
@@ -1,88 +1,78 @@
 import React from 'react';
-import useForm from './useForm';
 import validate from './validateInfo';
+import useForm from './useForm';
 import './Form.css';
 
-function FormSignup(submitForm) {
-    const { handleChange, values, handleSubmit, errors } 
-    = useForm(
-        submitForm,
-        validate
-    );
+const FormSignup = ({ submitForm }) => {
+  const { handleChange, handleSubmit, values, errors } = useForm(
+    submitForm,
+    validate
+  );
 
-    return (
-        <div>
-            <div className="form-content-right">
-               <form className="form" onSubmit={handleSubmit}>
-                   <h1>Get started with us today !  Create your account by filling out the information below .</h1>
-                   <div className="form-inputs">
-                        <label htlmFor="username" className="form-label">
-                            Username 
-                        </label>
-                        <input 
-                            id="username"
-                            type="text" 
-                            name="username" 
-                            className="form-input"
-                            placeholder="Enter your username"
-                            value={values.username}
-                            onChange={handleChange}
-                        />
-                        {errors.username && <p>{errors.username}</p>}
-                   </div>
-                   <div className="form-inputs">
-                        <label htlmFor="email" className="form-label">
-                            Email
-                        </label>
-                        <input 
-                            id="email"
-                            type="email" 
-                            name="email" 
-                            className="form-input"
-                            placeholder="Enter your email"
-                            value={values.email}
-                            onChange={handleChange}
-                        />
-                        {errors.email && <p>{errors.email}</p>}
-                   </div>
-                   <div className="form-inputs">
-                        <label htlmFor="password" className="form-label">
-                            Password
-                        </label>
-                        <input 
-                            id="password"
-                            type="password" 
-                            name="password" 
-                            className="form-input"
-                            placeholder="Enter your password"
-                            value={values.password}
-                            onChange={handleChange}
-                        />
-                        {errors.password && <p>{errors.password}</p>}
-                   </div>
-                   <div className="form-inputs">
-                        <label htlmFor="password2" className="form-label">
-                            Confirm Password
-                        </label>
-                        <input 
-                            id="password2"
-                            type="password" 
-                            name="password2" 
-                            className="form-input"
-                            placeholder="Enter your password2"
-                            value={values.password2}
-                            onChange={handleChange}
-                        />
-                        {errors.password2 && <p>{errors.password2}</p>}
-                   </div>
-                   <button className="form-input-btn" type="submit">Sign Up</button>
-                   <span className="form-input-login">
-                        Already have an account ? Login <a href="#"> here</a>
-                   </span>
-               </form>
-            </div>
+  return (
+    <div className='form-content-right'>
+      <form onSubmit={handleSubmit} className='form' noValidate>
+        <h1>
+          Get started with us today! Create your account by filling out the
+          information below.
+        </h1>
+        <div className='form-inputs'>
+          <label className='form-label'>Username</label>
+          <input
+            className='form-input'
+            type='text'
+            name='username'
+            placeholder='Enter your username'
+            value={values.username}
+            onChange={handleChange}
+          />
+          {errors.username && <p>{errors.username}</p>}
         </div>
-    )
-}
+        <div className='form-inputs'>
+          <label className='form-label'>Email</label>
+          <input
+            className='form-input'
+            type='email'
+            name='email'
+            placeholder='Enter your email'
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password'
+            placeholder='Enter your password'
+            value={values.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p>{errors.password}</p>}
+        </div>
+        <div className='form-inputs'>
+          <label className='form-label'>Confirm Password</label>
+          <input
+            className='form-input'
+            type='password'
+            name='password2'
+            placeholder='Confirm your password'
+            value={values.password2}
+            onChange={handleChange}
+          />
+          {errors.password2 && <p>{errors.password2}</p>}
+        </div>
+        <button className='form-input-btn' type='submit'>
+          Sign up
+        </button>
+        <span className='form-input-login'>
+          Already have an account? Login <a href='#'>here</a>
+        </span>
+      </form>
+    </div>
+  );
+};
 
 export default FormSignup;
